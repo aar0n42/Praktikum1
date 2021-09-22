f69b5b21721a4a143105aa7eda4b6fe3f68a9cf7
aar0n42 <91214823+aar0n42@users.noreply.github.com>
2021-09-22 19:12:05 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,6 @@
+class App{
+  
+	public static void main(String[] args){  
+		System.out.println("Jean_Herrera : Group_3");  
+	}  
+}  
---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM java:8
+
+WORKDIR /app
+
+COPY . /app
+
+RUN javac App.java
+
+CMD ["java", "App"]
